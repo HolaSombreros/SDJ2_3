68b494920eb86abc77b24a89fd2d2cc1c2fad748
stefaniatomuta <304173@via.dk>
2021-05-03 14:54:03 +0300

    ssdsads

---
+++ src/Deposit.java
@@ -0,0 +1,62 @@
+import utility.collection.ArrayList;
+import utility.collection.ListADT;
+import utility.collection.QueueADT;
+
+public class Deposit<T> implements QueueADT<T>
+{
+    private ListADT<T> list;
+
+    public Deposit(){
+        this.list = new ArrayList<>();
+    }
+    @Override
+    public synchronized void enqueue(T element)
+    {
+        list.add(list.size(), element);
+    }
+
+    @Override
+    public synchronized T dequeue()
+    {
+        return list.remove(0);
+    }
+
+    @Override
+    public synchronized T first()
+    {
+        return list.get(0);
+    }
+
+    @Override
+    public synchronized int indexOf(T element)
+    {
+        return list.indexOf(element);
+    }
+
+    @Override
+    public synchronized boolean isEmpty()
+    {
+        return list.isEmpty();
+    }
+
+    @Override
+    public synchronized boolean isFull()
+    {
+        return list.isFull();
+    }
+
+    @Override
+    public synchronized int size()
+    {
+        return list.size();
+    }
+
+    @Override
+    public synchronized int capacity()
+    {
+        return -1;
+    }
+    public synchronized String toString(){
+        return list.toString();
+    }
+}
